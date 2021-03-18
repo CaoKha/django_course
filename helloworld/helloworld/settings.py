18a8b971573ee176e7a0b23d933fa6196eb17c87
CaoKha <nguyencaokha131995@gmail.com>
2021-03-18 15:01:02 +0100

    commit

--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'say_hello_world',
+    'newyear',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- helloworld/say_hello_world/views.py
+++ helloworld/say_hello_world/views.py
@@ -3,5 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "sayhello/index.html")
+
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+
+def greet(request, name):
+    return render(request, "sayhello/greet.html", {
+        "name": name.capitalize()
+    })
