876da14ba51de7aeb90f70333b7b7d4f1fdd56c7
Kha Nguyen Cao <nguyencaokha131995@gmail.com>
2021-03-22 15:24:13 +0100

    commit

--- helloworld/tasks/views.py
+++ helloworld/tasks/views.py
@@ -1,8 +1,59 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo","bar","baz"]
-# Create your views here.
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
+
+tasks = ["foo", "bar", "baz"]
+# def index(request):
+#     return render(request, "tasks/index.html", {
+#         "tasks": tasks
+#     })
+
+
+# def add(request):
+#     return render(request, "tasks/add.html", {
+#         "form":  NewTaskForm()
+#     })
+
+
 def index(request):
-    return render(request,"tasks/index.html",{
-        "tasks" : tasks
+    # Check if there already exists a "tasks" key in our session
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
+    return render(request, "tasks/index.html", {
+        "tasks": request.session["tasks"]
+    })
+
+
+def add(request):
+    # Check if method is POST
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewTaskForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            task = form.cleaned_data["task"]
+
+            # Add the new task to out list of tasks
+            tasks.append(task)
+
+            # Redirect user to list of tasks
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            # If the form is invalid, re-render the page with existing information
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
     })
