310bb8a1d41a6c0a8b00337807f5627c023c1f51
Kha Nguyen Cao <nguyencaokha131995@gmail.com>
2021-05-17 10:46:16 +0200

    correct valid flight

--- airline/flights/tests.py
+++ airline/flights/tests.py
@@ -57,7 +57,7 @@ class FlightTestCase(TestCase):
         f = Flight.objects.get(origin=a1, destination=a1)
         c = Client()
         response = c.get(f"/flights/{f.id}")
-        self.assertEqual(response.status_code,500)
+        self.assertEqual(response.status_code,200)
 
     def test_invalid_flight_page(self):
         max_id = Flight.objects.all().aggregate(Max('id'))["id__max"]
